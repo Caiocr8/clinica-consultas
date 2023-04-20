8fcb104d726064a5de9acd028296e5755cc15894
Caiocr8 <ccaiocr@gmail.com>
2023-04-20 15:43:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <div className=' flex justify-center text-xl text-yellow-300'>
+        <p>HELLO WORLD</p>
+      </div>
+    </>
+  )
+}
+
+export default App
